34a7cc40d164d4a4db2d157ef019bcc5f4515064
Jooho Yoon <platonicojju@gmail.com>
2021-05-06 15:38:01 +0900

    Feat: 메인 기능 추가
    Pull Request 테스트를 위한 소스 추가

---
+++ src/main/java/test/git/com/main/Main.java
@@ -0,0 +1,12 @@
+package test.git.com.main;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("이 코드는 eGov에서 작성한 문서입니다.");
+		System.out.println("1. 소스를 Clone한 후 아래에 코드를 추가하십시오.");
+		System.out.println("=========[코드 추가 시작]=========");
+		System.out.println("=========[코드 추가    끝]=========");
+		System.out.println("2. 코드 추가 후 다시 Pull Request를 보내십시오.");
+	}
+
+}
